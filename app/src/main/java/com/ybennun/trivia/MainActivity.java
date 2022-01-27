d20c92d57b94265bc7b09518bbc57a007e050ab2
YeshayahuBennun <ybennun@yahoo.com>
2022-01-27 19:14:36 +0200

    commit#10 - Trivia App - Finalizing Animations

--- app/src/main/java/com/ybennun/trivia/MainActivity.java
+++ app/src/main/java/com/ybennun/trivia/MainActivity.java
@@ -3,7 +3,9 @@ package com.ybennun.trivia;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
@@ -52,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         int snackMessageId = 0;
         if (userChoseCorrect == answer) {
             snackMessageId = R.string.correct_answer;
+            fadeAnimation();
         } else {
             snackMessageId = R.string.incorrect_answer;
             shakeAnimation();
@@ -66,6 +69,32 @@ public class MainActivity extends AppCompatActivity {
         binding.textViewOutOf.setText(String.format(getString(R.string.text_formatted), currentQuestionIndex, questionArrayList.size()));
     }
 
+    private void fadeAnimation(){
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f,1.0f);
+        alphaAnimation.setDuration(300);
+        alphaAnimation.setRepeatCount(1);
+        alphaAnimation.setRepeatMode(Animation.REVERSE);
+
+        binding.cardView.setAnimation(alphaAnimation);
+
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                binding.questionTextView.setTextColor(Color.GREEN);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                binding.questionTextView.setTextColor(Color.WHITE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
     private void updateQuestion() {
         String question = questionList.get(currentQuestionIndex).getAnswer();
         binding.questionTextView.setText(question);
@@ -76,5 +105,22 @@ public class MainActivity extends AppCompatActivity {
         Animation shake = AnimationUtils.loadAnimation(MainActivity.this, R.anim.shake_animation);
 
         binding.cardView.setAnimation(shake);
+
+        shake.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                binding.questionTextView.setTextColor(Color.RED);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                binding.questionTextView.setTextColor(Color.WHITE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }
